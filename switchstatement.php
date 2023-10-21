c1f37b3a848f7005ab78861cd580bda1de6b06e8
Princessmaxine <94942829+Princessmaxine@users.noreply.github.com>
2023-10-21 14:34:57 -0500

    saving to github

--- switchstatement.php
+++ switchstatement.php
@@ -4,16 +4,16 @@
   ?>
   <h1>Switch Statements</h1>  
   <?php
-  $grade = 'A';
-  switch(grade){
+  $grade = 'B';
+  switch($grade){
     case 'A':
-        echo '<h2 style = "color: green">YOU ARE A SUPERSTAR!</h2>';
+        echo '<h2 style ="color: green">YOU ARE A SUPERSTAR!</h2>';
         break;
     case 'B':
-         echo '<h2 style = "color: blue">YOU DID WELL!</h2>'; 
+         echo '<h2 style ="color: blue">YOU DID WELL!</h2>'; 
          break;
     default:
-        echo '<h2 style = "color: red">YOU HAVE FAILED....</h2>';          
+        echo '<h2 style ="color: red">YOU HAVE FAILED....</h2>';          
   }
 
   ?>
